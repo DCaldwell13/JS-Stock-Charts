a57968f0251707ec34d0e9f0c363901dc9fb6181
DCaldwell13 <draykcaldwell0516@gmail.com>
2023-04-13 21:10:46 -0700

    JS-Stock-Charts

--- public/index.js
+++ public/index.js
@@ -3,7 +3,46 @@ async function main() {
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+   // api_key = 7ca3566aaf504cc9baf072be07a581e1
+    fetch('https://github.com/evzaboun/twelvedata.git')
+
+    const { GME, MSFT, DIS, BNTX } = mockData;
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    let updateMain = await Response.json(timeChartCanvas);
+    console.log(updateMain);
 
 }
 
 main()
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+
+
+//how to use response.json()?
+
+
+ /*   let GME = result.GME
+    let MSFT = result.MSFT
+    let DIS = result.DIS
+    let BTNX = result.BTNX*/
+
+
+// Bonus Note: 
+// Another way to write the above lines would to refactor it as:
+   // const {GME, MSFT, DIS, BTNX} = result 
+// This is an example of "destructuring" an object
+// "Destructuring" creates new variables from an object or an array
